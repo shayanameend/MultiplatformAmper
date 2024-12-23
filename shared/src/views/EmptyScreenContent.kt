232514abdbf8c0f2b1cfb01c0ec97f37ffe70647
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-12-23 16:56:28 +0500

    Using Drawable Resource

--- shared/src/views/EmptyScreenContent.kt
+++ shared/src/views/EmptyScreenContent.kt
@@ -1,10 +1,13 @@
 package views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import shared.generated.resources.Res
+import shared.generated.resources.content_based_color_scheme_1
 
 @Composable
 fun EmptyScreenContent(
@@ -14,6 +17,9 @@ fun EmptyScreenContent(
     modifier = modifier,
     contentAlignment = Alignment.Center,
   ) {
-    Text("No Data Available")
+    Image(
+      painter = painterResource(Res.drawable.content_based_color_scheme_1),
+      contentDescription = null,
+    )
   }
 }
