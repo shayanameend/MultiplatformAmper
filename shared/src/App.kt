782b8cadd74eaa79dab00f63f1c8c2a4e3b5682a
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-12-24 15:48:12 +0500

    Simple App Scaffold

--- shared/src/App.kt
+++ shared/src/App.kt
@@ -1,30 +1,100 @@
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.TextSnippet
+import androidx.compose.material.icons.filled.FormatPaint
+import androidx.compose.material.icons.filled.Opacity
+import androidx.compose.material.icons.filled.Widgets
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onGloballyPositioned
 import theme.AppTheme
 
+const val narrowScreenWidthThreshold = 1300
+
+data class Screen(
+  val title: String,
+  val icon: ImageVector,
+  val content: @Composable () -> Unit,
+)
+
+val screens = listOf(
+  Screen("Components", Icons.Filled.Widgets) {},
+  Screen("Color", Icons.Filled.FormatPaint) {},
+  Screen("Typography", Icons.AutoMirrored.Filled.TextSnippet) {},
+  Screen("Elevation", Icons.Filled.Opacity) {},
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() = AppTheme {
-  val contentIndexState = mutableStateOf(0)
+  var screenWidth by remember { mutableStateOf(0) }
+
+  val snackbarHostState = remember { SnackbarHostState() }
+  var selectedScreen by remember { mutableStateOf(screens[0]) }
+  val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
   Scaffold(
-    topBar = {
-      AppTopBar(
-        contentIndexState = contentIndexState,
-      )
+    modifier = Modifier
+      .nestedScroll(scrollBehavior.nestedScrollConnection)
+      .onGloballyPositioned {
+        screenWidth = it.size.width
+      },
+    snackbarHost = {
+      SnackbarHost(hostState = snackbarHostState)
     },
-    content = { safePadding ->
-      AppContent(
-        modifier = Modifier.padding(safePadding),
-        contentIndexState = contentIndexState,
-      )
+    topBar = {
+      @Composable
+      fun title() {
+        Text("Multiplatform App")
+      }
+
+      @Composable
+      fun actions() {
+        IconButton(
+          onClick = {}
+        ) {}
+
+        IconButton(
+          onClick = {}
+        ) {}
+      }
+
+      if (screenWidth <= narrowScreenWidthThreshold) {
+        TopAppBar(
+          title = { title() },
+          scrollBehavior = scrollBehavior,
+          actions = { actions() },
+        )
+      } else {
+        CenterAlignedTopAppBar(
+          title = { title() },
+          scrollBehavior = scrollBehavior,
+          actions = { actions() },
+        )
+      }
     },
     bottomBar = {
-      AppBottomBar(
-        contentIndexState = contentIndexState,
-      )
+      if (screenWidth <= narrowScreenWidthThreshold) {
+        NavigationBar {
+          screens.forEach { screen ->
+            NavigationBarItem(
+              icon = { Icon(screen.icon, contentDescription = screen.title) },
+              label = { Text(screen.title) },
+              selected = selectedScreen == screen,
+              onClick = {
+                scrollBehavior.state.contentOffset = 0f
+                selectedScreen = screen
+              }
+            )
+          }
+        }
+      }
     },
-  )
+  ) {
+    selectedScreen.content()
+  }
 }
+
+//internal expect fun openUrl(url: String)
